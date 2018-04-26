d957ccc75a16e8cc1df7b7f11122e5425dd999ad
DivyanshuSaxena <dssaxena2011@gmail.com>
2018-04-26 23:42:28 +0530

    Simple cases working with extended substitutions

--- assignment6.ml
+++ assignment6.ml
@@ -3,7 +3,7 @@ exception TypeMismatch;;
 exception NotUnifiable;; 
 open List;;
 type symbol = List;;
-type term = Const of string | Var of string * int | FuncSym of symbol * (term list);;
+type term = Const of string | Var of string | FuncSym of symbol * (term list);;
 type atom = PredSym of string * (term list) | Cut | Fail;;
 type clause = Fact of atom | Rule of (atom * (atom list)) | Query of (atom list);;
 type answer = Map of (string * term) list | Claim of bool;;
@@ -17,12 +17,12 @@ let rec fold_left_concat f e l = match l with
 					| hd::tl -> fold_left_concat f (e@(f hd)) tl;;
 
 let rec search substlist a = match substlist with
-					| (e,n,t)::tl -> if (e,n)=a then t else search tl a
+					| (e,t)::tl -> if e=a then t else search tl a
 					| [] -> raise TypeMismatch;;
 
 let rec substitute s t = match t with
 				| Const s -> Const s
-				| Var a -> let ispresent e = (match e with (v,n,t) -> if (v,n)=a then true else false) in
+				| Var a -> let ispresent e = (match e with (v,t) -> if v=a then true else false) in
 						let orfunc a b = a || b in 
 						(if (fold_left orfunc false (map ispresent s)) then (search s a) else (Var a))
 				| FuncSym (sym,l) -> FuncSym (sym, map (substitute s) l);;
@@ -33,7 +33,7 @@ let rec subst sigma atom = match atom with
 					| Fail -> Fail;;
 
 let rec subst_compose s1 s2 l = match s1 with
-						| (v,n,t)::tl -> subst_compose tl s2 (l@[(v,n,substitute s2 t)]) 
+						| (v,t)::tl -> subst_compose tl s2 (l@[(v,substitute s2 t)]) 
 						| [] -> let rec ispresent l e = (match l with 
 														hd::tl -> if ((fst hd)=(fst e) && (snd hd)=(snd e)) then [] else (ispresent tl e) | [] -> [e]) in
 								let concat a b = a@b in 
@@ -53,25 +53,23 @@ let rec vars term = match term with
 					| FuncSym (s,l) -> fold_left_concat vars [] l;;
 
 let rec mgu t1 t2 = match t1 with
-					| Var (a,n) -> if t1=t2 then [] else (let rec search l e = (match l with hd::tl -> (if hd=e then true else false) | [] -> false) in
-							if not (search (vars t2) (a,n)) then [(a,n,t2)] else raise NotUnifiable)
+					| Var (a) -> if t1=t2 then [] else [(a,t2)]
 					| Const s -> if t1=t2 then [] else (mgu t2 t1)
 					| FuncSym (List, l) -> (match t2 with
 								| Var a -> mgu t2 t1 | Const s2 -> raise NotUnifiable
 								| FuncSym (List, l2) -> let rec mgulist l1 l2 = (match (l1,l2) with
 										| (x1::xs1,x2::xs2) -> let sigma = mgu x1 x2 in compose sigma (mgulist (map (substitute sigma) xs1) (map (substitute sigma) xs2))
 										| ([],xs) -> let isvar e = (match e with | (Var a) -> true | _ -> false) in 
-													let mapempty e = (match e with | (Var (a,n)) -> (a,n,FuncSym (List, [])) | _ -> raise NotUnifiable) in
+													let mapempty e = (match e with | (Var a) -> (a,FuncSym (List, [])) | _ -> raise NotUnifiable) in
 													let andf a b = a && b in
 													(if (fold_left andf true (map isvar xs)) then (map mapempty xs) else raise NotUnifiable) 
-										| (xs,[]) -> mgulist l2 l1
-										| _ -> raise NotUnifiable) 
-									in (mgulist l l2)
-								| _ -> raise NotUnifiable)
+										| (xs,[]) -> mgulist l2 l1 ) 
+									in (mgulist l l2) );;
+					(* 			| _ -> raise NotUnifiable);;
 					| FuncSym (s,l) -> (match t2 with
 								| Var a -> mgu t2 t1 | Const s2 -> raise NotUnifiable
 								| FuncSym (s2,l2) -> if (s=s2 && (length l)=(length l2)) then (fold_duo compose mgu [] l l2) 
-									else raise NotUnifiable);;
+									else raise NotUnifiable);; *)
 
 let rec unify atom1 atom2 = match (atom1,atom2) with
 					| (PredSym (s1,terml1),PredSym (s2,terml2)) -> (try
@@ -86,15 +84,17 @@ let rec matchgoal cl goal goals = (match cl with
 							| Rule (h,b) -> let sigma = (unify h goal) in (if sigma=[] then [] else (map (subst sigma) b@goals))
 							| _ -> raise TypeMismatch );;
 
-let rec evaluate goals program remrules subs = match goals with
+let rec evaluate goals program subs = match goals with
 					| [] -> subs
 					| currgoal::gl ->  let result = [] in
-						let rec goalloop prg rem goal goals i = (match rem with
+						let rec goalloop prg rem goal goals = (match rem with
 							| cl::tl -> let sigma = (match cl with | Fact h -> (unify h goal) | Rule (h,b) -> (unify h goal) | _ -> raise TypeMismatch) in
-										let remgoals = matchgoal cl goal goals in (match remgoals with
+										(match sigma with
 											| [] -> goalloop prg tl goal goals
-											| _ -> result@(evaluate remgoals prg prg (compose sigma subs)) )
-							| [] -> result) in (goalloop program program currgoal gl 0);;
+											| _ -> let remgoals = matchgoal cl goal goals in (match remgoals with
+														| [] -> (compose sigma subs)
+														| _ -> (evaluate remgoals prg (compose sigma subs)) ))
+							| [] -> []) in (goalloop program program currgoal gl);;
 
 (* Test Cases *)
 let goal1 = PredSym ("append", [FuncSym (List, [Const "1"; FuncSym (List, [Const "2"])]); FuncSym (List, [Const "3"; FuncSym (List, [Const "4"])]); Var "x"]);;
@@ -120,19 +120,26 @@ let prgm = [
 		)]
 	)];;
 unify facth goal1;;
-unify facth goal2;;
+(* unify facth goal2;; *)
 let sigma1 = unify ruleh goal1;;
-let sigma2 = unify ruleh goal2;;
-let pass = compose sigma1 [];;
+(* let sigma2 = unify ruleh goal2;; *)
+let pass1 = compose sigma1 [];;
+(* let pass2 = compose sigma2 [];; *)
 let remgoals1 = matchgoal rule goal1 [];;
-let remgoals2 = matchgoal rule goal2 [];;
+(* let remgoals2 = matchgoal rule goal2 [];; *)
 let remgoal1 = hd remgoals1;;
-let remgoal2 = hd remgoals2;;
-unify facth remgoal2;;
-unify ruleh remgoal2;;
-evaluate remgoals1 prgm prgm pass;; 
-evaluate [goal1] prgm prgm [];;
-evaluate [goal2] prgm prgm [];;
+(* let remgoal2 = hd remgoals2;; *)
+unify facth remgoal1;;
+(* unify facth remgoal2;; *)
+unify ruleh remgoal1;;
+matchgoal rule remgoal1 [];;  
+(* matchgoal (Fact facth) remgoal2 [];;   *)
+(* evaluate [goal1] prgm [];; *)
+(* evaluate [goal2] prgm [];; *)
 
 (* append([],L,L).
 append([X|Xs],L,[X|L2]) :- append(Xs,L,L2). *)
+
+(* let remgoals = matchgoal cl goal goals in (match remgoals with
+| [] -> goalloop prg tl goal goals
+| _ -> (evaluate remgoals prg prg (compose sigma subs)) ) *)
